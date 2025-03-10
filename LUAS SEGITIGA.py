a31b6cf5cc7daa48b5cfbb81c834208e598dd596
ChandaKarimaPasifika <chandakarimapasifika@gmail.com>
2025-03-10 23:58:15 +0700

    Delete LUAS SEGITIGA.py

--- LUAS SEGITIGA.py
+++
@@ -1,9 +0,0 @@
-# Program 1: Menghitung Luas Segitiga
-# Menggunakan input langsung dari pengguna
-def luas_segitiga():
-    alas = float(input("Masukkan panjang alas segitiga: "))
-    tinggi = float(input("Masukkan tinggi segitiga: "))
-    luas = 0.5 * alas * tinggi
-    print(f"Luas segitiga adalah: {luas}")
-
-luas_segitiga()
