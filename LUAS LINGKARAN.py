cbc8fb81aa261ac5896af478aafee26f5667d331
ChandaKarimaPasifika <chandakarimapasifika@gmail.com>
2025-03-10 23:57:48 +0700

    Delete LUAS LINGKARAN.py

--- LUAS LINGKARAN.py
+++
@@ -1,9 +0,0 @@
-# Program 4: Menghitung Luas Lingkaran
-# Menggunakan fungsi
-import math
-
-def luas_lingkaran(radius):
-    return math.pi * radius ** 2
-
-radius = 7
-print(f"Luas lingkaran adalah: {luas_lingkaran(radius)}")
