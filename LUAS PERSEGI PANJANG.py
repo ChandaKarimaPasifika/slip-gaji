a1ed1e997b6d2d3e22c677a50658c92c22a39214
ChandaKarimaPasifika <chandakarimapasifika@gmail.com>
2025-03-10 23:58:02 +0700

    Delete LUAS PERSEGI PANJANG.py

--- LUAS PERSEGI PANJANG.py
+++
@@ -1,8 +0,0 @@
-# Program 2: Menghitung Luas Persegi Panjang
-# Menggunakan fungsi dengan parameter
-def luas_persegi_panjang(panjang, lebar):
-    return panjang * lebar
-
-panjang = 10
-lebar = 5
-print(f"Luas persegi panjang adalah: {luas_persegi_panjang(panjang, lebar)}")
